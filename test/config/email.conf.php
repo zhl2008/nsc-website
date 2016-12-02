6e9bf79584f180c4291215221bc27321864f9843
zhl2008 <zhl2008vvvss@163.com>
2016-12-02 19:30:01 +0800

    Update email.conf.php

--- test/config/email.conf.php
+++ test/config/email.conf.php
@@ -1,7 +1,7 @@
 <?php
 $smtpserver = "mail.bit.edu.cn";//SMTP服务器
 $smtpserverport =25;//SMTP服务器端口
-$smtpusermail = "1120131205@bit.edu.cn";//SMTP服务器的用户邮箱
-$smtpuser = "1120131205";//SMTP服务器的用户帐号
-$smtppass = "Srandcr";//SMTP服务器的用户密码
+$smtpusermail = "nobody@body.com";//SMTP服务器的用户邮箱
+$smtpuser = "nobody";//SMTP服务器的用户帐号
+$smtppass = "anonymous";//SMTP服务器的用户密码
 ?>
